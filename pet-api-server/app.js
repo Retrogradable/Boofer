7d71a18f225918f03607f161a8643141510a9393
Code-Contemporary <rsupple@protonmail.com>
2023-03-16 20:03:37 -0700

    Added components, .env file
    Added some initial components to the frontend and created a .env file for API keys. So difficult, I'm sweating.

--- pet-api-server/app.js
+++ pet-api-server/app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 1337
 
+require('dotenv').config()
+
+const apiKey = process.env.API_KEY
+
 app.get('/', (req, res) => {
   res.send('Hello Doggos!')
 })
