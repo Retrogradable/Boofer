e9a23c6d0270275ab3e41297431cb02d512ea434
Code-Contemporary <rsupple@protonmail.com>
2023-03-17 20:57:02 -0700

    Added auth code
    Added code to get the Petfinder API access tokens for API requests using Fetch, because who needs to waste their time with Axios anymore.

--- pet-api-server/app.js
+++ pet-api-server/app.js
@@ -5,6 +5,30 @@ const port = 1337
 require('dotenv').config()
 
 const apiKey = process.env.API_KEY
+const apiSecret = process.env.API_SECRET
+const tokenURL = 'https://api.petfinder.com/v2/oauth2/token'
+let accessToken = ''
+
+// Get the auth token from Petfinder with Fetch API because who needs Axios anymore xD
+const getAuthToken = async () => {
+  const res = await fetch(tokenURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: `grant_type=client_credentials&client_id=${apiKey}&client_secret=${apiSecret}`
+  })
+
+  if (res.ok) {
+    const data = await res.json()
+    accessToken = data.access_token
+    
+    console.log(data)
+  } else {
+    console.log(`Petfinder API request failed: ${res.status}`)
+  }
+}
+
 
 app.get('/', (req, res) => {
   res.send('Hello Doggos!')
